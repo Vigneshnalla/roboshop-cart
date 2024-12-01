5013d66106e16e127adbde9af46980cfaf5c1a07
vignesh nalla <vigneshnalla999@gmail.com>
2024-12-01 12:09:01 +0530

    ...

--- services/productService.js
+++ services/productService.js
@@ -4,7 +4,7 @@ const { catalogueHost, cataloguePort } = require('../config/config');
 function getProduct(sku) {
     return new Promise((resolve, reject) => {
         request(
-            `http://${catalogueHost}:${cataloguePort}/product/${sku}`,
+            `http://${catalogueHost}:${cataloguePort}/api/product/${sku}`,
             (err, res, body) => {
                 if (err) reject(err);
                 else if (res.statusCode !== 200) resolve(null);
